7a734e3da6ad8e05624933f219141f2935d029ce
Mubeen Ameen <mubeenamin6@gmail.com>
2022-10-06 22:13:15 +0500

    Javascript

---
+++ Q3.js
@@ -0,0 +1,8 @@
+const name = 'Mubeen Ameen';
+const name1 = 'mubeen';
+//name print in lower case
+console.log(name.toLowerCase());
+//name print in Upper case
+console.log(name.toUpperCase());
+//name print in Title case
+console.log(name1.charAt(0).toUpperCase());
